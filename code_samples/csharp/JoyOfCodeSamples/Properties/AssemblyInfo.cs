17dd2faa0dac287bc937f7ed75810258bf97a7c3
Llewellyn Falco <isidore@setgame.com>
2018-06-06 18:57:38 +0300

    csharp

---
+++ code_samples/csharp/JoyOfCodeSamples/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("JoyOfCodeSamples")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("JoyOfCodeSamples")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d5f85cd8-4096-4ec9-946e-65ed4596d41a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ code_samples/csharp/JoyOfCodeSamples/TestLambda.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace JoyOfCodeSamples
+{
+    [TestClass]
+    public class TestLambda
+    {
+        [TestMethod]
+        public void Test()
+        {
+            int cake = 50;
+            {
+                /* Introduce.Lambda.Example.1 */
+                //Original
+                int eat = cake - (1 / 2) * cake;
+                /* end */
+            }
+            {
+                /* Introduce.Lambda.Example.2 */
+                // Introduce Lambda
+                int eat = ((Func<int>) (() => cake - (1 / 2) * cake))();
+                /* end */
+            }
+            {
+                /* Introduce.Lambda.Example.3 */
+                // Introduce Lambda parameter
+                int eat = ((Func<int, int>) (c => c - (1 / 2) * c))(cake);
+                /* end */
+            }
+        }
+    }
+}
